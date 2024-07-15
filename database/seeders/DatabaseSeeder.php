58a430d9ad3204316e303daf7cce08454ae0b662
Imran Khamis <mitch@Imrans-MacBook-Pro.local>
2024-07-16 02:01:08 +0300

    fixed seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            JobCategory::class,
+            JobCategorySeeder::class,
             VisaTypeSeeder::class
 
         ]);
